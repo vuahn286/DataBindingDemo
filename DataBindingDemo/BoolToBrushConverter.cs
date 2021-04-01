0ab692838be270ad1c60e2b4753286f8a5c8e92c
ПП ПППП ПП <ledinhvu2806@gmail.com>
2021-04-01 23:16:57 +0300

    Add project files.

---
+++ DataBindingDemo/BoolToBrushConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Data;
+using System.Windows.Media;
+using System.Globalization;
+using System;
+
+namespace DataBindingDemo
+{
+    [ValueConversion(typeof(bool), typeof(Brush))]
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Brush b = null;
+            if (value != null &&
+            value.GetType() == typeof(bool))
+            {
+                b = (bool)value ? Brushes.Green : Brushes.Red;
+            }
+            return b;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ DataBindingDemo/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingDemo
+{
+    class Student
+    {
+        public string StudentName { get; set; }
+        public bool IsEnrolled { get; set; }
+    }
+}
